52fd7581f528750cd1bcecb2494ef902ae8c2428
ubik97 <43785266+ubik97@users.noreply.github.com>
2019-03-18 10:41:57 -0400

    Update pitchtest.js

--- pitchtest.js
+++ pitchtest.js
@@ -1,40 +1,41 @@
-var c = 261.63;
-var d = 293.67;
-var e = 329.63;
-var f = 349.23;
-var g = 392;
-var a = 440;
-var b = 493.88;
-var cSharp = 277.18;
-var dSharp = 311.13;
-var fSharp = 369.99;
-var gSharp = 415.3;
-var aSharp = 466.16;
-var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-var oscillator = audioCtx.createOscillator();
-
 function start() {
-  let idx = Math.ceil(Math.random(1, 12));
+  document.getElementById('start').disabled = true;
+  noteGenerator();
+}
+
+function noteGenerator() {
   let noteArr = [261.63, 293.67, 329.63, 349.23, 392, 440, 493.88, 277.18, 311.13, 369.99, 415.3, 466.16];
+              //[c,       d,      e,      f,     g,   a,   b,      c#,      d#,    f#,     g#,    a#     ]
+  let idx = randomInt(0, 11);
+  console.log(idx);
   let correct = noteArr[idx];
   console.log(correct);
   playFreq(correct);
 }
 
-function notePlayed(note, correct) {
-  playFreq(note);
-  compare(note, correct);
-  newNote();
+//random number generator
+function randomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+
+}
+
+function notePlayed(note) {
+playFreq(note);
+oscillator.disconnect(audioCtx.destination);
 }
 
 function playFreq(freq) {
-    var dur = 1;
+    var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+    var oscillator = audioCtx.createOscillator();
+    var dur = 0.5;
     var stopVal = 10;
     console.log(freq);
 
     oscillator.type = 'sine';
     oscillator.frequency.value = freq;
-    oscillator.gain = 0.5;
+    oscillator.gain = 0.1;
     oscillator.connect(audioCtx.destination);
 
     oscillator.onended = stopVal;
